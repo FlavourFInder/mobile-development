cb364784c15c1d9f29b846cccc2d748281eaaff5
[username] <[user>
2024-05-29 02:25:28 +0700

    - mengganti file gambar register untuk menghilangkan error - membuat textview di page register dan sign in bisa di click

--- app/src/main/java/com/example/flavorfinder/view/RegisterActivity.kt
+++ app/src/main/java/com/example/flavorfinder/view/RegisterActivity.kt
@@ -1,21 +1,33 @@
 package com.example.flavorfinder.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.flavorfinder.R
+import com.example.flavorfinder.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        setUpTextViewButton()
+    }
+
+    private fun setUpTextViewButton(){
+        binding.GoToSignInPageTextButton.setOnClickListener {
+            startActivity(Intent(this, SigninActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/example/flavorfinder/view/SigninActivity.kt
+++ app/src/main/java/com/example/flavorfinder/view/SigninActivity.kt
@@ -1,21 +1,33 @@
 package com.example.flavorfinder.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.flavorfinder.R
+import com.example.flavorfinder.databinding.ActivitySigninBinding
 
-class LoginActivity : AppCompatActivity() {
+class SigninActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySigninBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signin)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        setUpTextViewButton()
+    }
+
+    private fun setUpTextViewButton(){
+        binding.GoToRegisterPageTextButton.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
 }
