6de0aee7021344f46fa7b618728e31dc4a36dcae
[username] <[user>
2024-06-03 11:18:57 +0700

    - mengimplementasikan firebase auth ke sign in

--- app/src/main/java/com/example/flavorfinder/view/SigninActivity.kt
+++ app/src/main/java/com/example/flavorfinder/view/SigninActivity.kt
@@ -2,15 +2,18 @@ package com.example.flavorfinder.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.flavorfinder.R
 import com.example.flavorfinder.databinding.ActivitySigninBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SigninActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySigninBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySigninBinding.inflate(layoutInflater)
@@ -21,6 +24,7 @@ class SigninActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        firebaseAuth = FirebaseAuth.getInstance()
 
         setUpTextViewButton()
         setUpButton()
@@ -34,7 +38,20 @@ class SigninActivity : AppCompatActivity() {
 
     private fun setUpButton(){
         binding.loginButton.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            startActivity(Intent(this, MainActivity::class.java))
+                        }else{
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else{
+                Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
