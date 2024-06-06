02b328774f68b60c2ef9c5c079c8e6edc4bc2d98
Febi Fiantika <febifian23@gmail.com>
2024-06-07 00:52:59 +0800

    mengimplementasikan method POST pada fitur bookmark

--- app/src/main/java/com/example/flavorfinder/network/retrofit/AuthApiService.kt
+++ app/src/main/java/com/example/flavorfinder/network/retrofit/AuthApiService.kt
@@ -1,11 +1,17 @@
 package com.example.flavorfinder.network.retrofit
 
-import com.example.flavorfinder.network.response.LoginData
+import com.example.flavorfinder.network.response.GetBookmarkResponse
 import com.example.flavorfinder.network.response.LoginResponse
+import com.example.flavorfinder.network.response.PostBookmarkResponse
 import com.example.flavorfinder.network.response.RegisterResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface AuthApiService {
     @FormUrlEncoded
@@ -22,4 +28,20 @@ interface AuthApiService {
         @Field("identifier") identifier: String,
         @Field("password") password: String
     ): LoginResponse
+
+    @GET("bookmark")
+    suspend fun getBookmark(): GetBookmarkResponse
+
+    @POST("bookmark")
+    suspend fun addBookmark(
+        @Header("Authorization") token: String,
+        @Body recipeId: Map<String, Int>
+    ): PostBookmarkResponse
+
+    @DELETE("bookmark/{bookmarkId}")
+    suspend fun deleteBookmark(
+        @Header("Authorization") token: String,
+        @Path("bookmarkId") bookmarkId: String
+    ): PostBookmarkResponse
+
 }
--- app/src/main/java/com/example/flavorfinder/view/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/flavorfinder/view/ui/detail/DetailViewModel.kt
@@ -3,8 +3,33 @@ package com.example.flavorfinder.view.ui.detail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.flavorfinder.helper.Result
+import com.example.flavorfinder.network.repository.MealRepository
+import com.example.flavorfinder.network.response.PostBookmarkResponse
+import kotlinx.coroutines.launch
 
-class DetailViewModel: ViewModel() {
-    private val _isBookmark = MutableLiveData<Boolean>()
-    val isBookmark: LiveData<Boolean> = _isBookmark
+class DetailViewModel(private val repository: MealRepository): ViewModel() {
+
+    private val _bookmarkResult = MutableLiveData<Result<PostBookmarkResponse>>()
+    val bookmarkResult: LiveData<Result<PostBookmarkResponse>> = _bookmarkResult
+
+    private val _bookmarkStatus = MutableLiveData<Boolean>()
+    val bookmarkStatus: LiveData<Boolean> = _bookmarkStatus
+
+    fun addBookmark(recipeId: Int) {
+        viewModelScope.launch {
+            repository.addBookmark(recipeId).observeForever {
+                _bookmarkResult.value = it
+            }
+        }
+    }
+
+//    fun deleteBookmark(bookmarkId: String) {
+//        viewModelScope.launch {
+//            repository.deleteBookmark(bookmarkId).observeForever {
+//                _bookmarkResult.value = it
+//            }
+//        }
+//    }
 }
