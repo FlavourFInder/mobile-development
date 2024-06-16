bdd6239f7811628bb0a88453e1d6c5978d21ff25
[username] <[user>
2024-06-16 11:16:52 +0700

    - menambah meal paging source dan noopListUpdateCallback pada HomeFragmentTest.kt

--- app/src/test/java/com/example/flavorfinder/view/ui/home/HomeFragmentTest.kt
+++ app/src/test/java/com/example/flavorfinder/view/ui/home/HomeFragmentTest.kt
@@ -1,11 +1,17 @@
 package com.example.flavorfinder.view.ui.home
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import androidx.recyclerview.widget.ListUpdateCallback
 import com.example.flavorfinder.network.repository.MealRepository
 import com.example.flavorfinder.network.response.MealsItem
+import com.example.flavorfinder.network.response.MealsResponse
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
@@ -57,5 +63,31 @@ class HomeFragmentTest {
         } finally {
             homeViewModel.meal.removeObserver(observer)
         }
+    }
+
+    class MealPagingSource : PagingSource<Int, MealsItem>(){
+        companion object{
+//            di mealPagingSource asli seperti ini
+//            const val INITIAL_PAGE_INDEX = 1
+
+//            ini mengikuti contoh modul
+            fun snapshot(items: List<MealsItem>): PagingData<MealsItem>{
+                return PagingData.from(items)
+            }
+        }
+        override fun getRefreshKey(state: PagingState<Int, MealsItem>): Int {
+            return 0
+        }
+
+        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MealsItem> {
+            return LoadResult.Page(emptyList(), 0, 1)
+        }
+    }
+
+    val noopListUpdateCallback = object : ListUpdateCallback {
+        override fun onInserted(position: Int, count: Int) {}
+        override fun onRemoved(position: Int, count: Int) {}
+        override fun onMoved(fromPosition: Int, toPosition: Int) {}
+        override fun onChanged(position: Int, count: Int, payload: Any?) {}
     }
 }
